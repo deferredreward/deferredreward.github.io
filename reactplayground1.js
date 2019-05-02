75921a17acb758d7afa0c8edced32142ecc9cfac
deferredreward <github@abidinginhesed.com>
2019-05-02 16:42:46 -0500

    changed to simple jsx element from react homepage

--- reactplayground1.js
+++ reactplayground1.js
@@ -1,39 +1,14 @@
-class Clock extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {date: new Date()};
-  }
-
-  componentDidMount() {
-    this.timerID = setInterval(
-      () => this.tick(),
-      1000
-    );
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.timerID);
-  }
-
-  tick() {
-    this.setState({
-      date: new Date()
-    });
-  }
-
+class HelloMessage extends React.Component {
   render() {
     return (
       <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+        Hello {this.props.name}
       </div>
     );
   }
 }
 
-const domContainer = document.querySelector('#root');
-
 ReactDOM.render(
-  <Clock />,
-  domContainer
+  <HelloMessage name="James Bond" />,
+  document.getElementById('hello-example')
 );
